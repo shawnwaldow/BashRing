5242d1f5819d029af8bcb23a275be7d26d5e91d2
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-21 16:36:00 -0700

    Entered rough draft of all model objects except BoutWinner.

--- bring/predict_app/admin.py
+++ bring/predict_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Ring_User, Fighter
+from .models import Ring_User, Fighter, Method, Fight_Card, Bout, User_Prediction, Weight_Class
 # Register your models here.
 
 admin.site.register(Ring_User)
 admin.site.register(Fighter)
+admin.site.register(Method)
+admin.site.register(Fight_Card)
+admin.site.register(Bout)
+admin.site.register(User_Prediction)
+admin.site.register(Weight_Class)
--- bring/predict_app/models.py
+++ bring/predict_app/models.py
@@ -13,21 +13,7 @@ class Ring_User(models.Model):
 	accuracy = models.FloatField(default=1)
 	experience = models.PositiveIntegerField(default=0)
 	avatar = models.ImageField(default="static/menu_images/user1.png",upload_to="avatar_images")
-"""
 
-class User_Prediction(models.Model):
-	#Foreign Keys
-	bout_id = models.ForeignKey(Bout)
-	winner = models.ForeignKey(Fighter)
-	method = models.ForeignKey(Method)
-	ring_user_id = models.ForeignKey(Ring_User)
-	round_final = models.PositiveSmallIntegerField()
-	#Remake these as a const Likert 
-	confidence = models.PositiveIntegerField()
-	excitement = models.PositiveIntegerField()
-	attachment = models.PositiveIntegerField()
-	note = models.TextField(max_length=255)
-"""
 class Fighter(models.Model):
 	last_name = models.CharField(max_length=64, default=' ')
 	first_name = models.CharField(max_length=64, default=' ')
@@ -48,6 +34,7 @@ class Fighter(models.Model):
 	spice = models.FloatField(default=1)
 	batwings = models.PositiveIntegerField()
 	water = models.FloatField(default=1)
+	gender = models.BooleanField(default=True) #F=Female, T=Male
 
 class Method(models.Model):
 	#CONST
@@ -79,5 +66,65 @@ class Method(models.Model):
 class Fight_Card(models.Model):
 	title = models.CharField(max_length=127)
 	organization = models.CharField(max_length=64, default='UFC')
-	start_time = models.DateField()
-	complete = models.DateField()
+	start_time = models.DateTimeField()
+
+class Weight_Class(models.Model):
+	#CONST
+	#Women's Division
+	ATOM_W = 9
+	STRAW_W = 10
+	FLY_W = 11
+	BANTAM_W = 12
+	FEATHER_W = 13
+
+	#Men's Division
+	FLY_M = 1
+	BANTAM_M = 2
+	FEATHER_M = 3
+	LIGHT_M = 4
+	WELTER_M = 5
+	MIDDLE_M = 6
+	LIGHT_HEAVY_M = 7
+	HEAVY_M = 8
+
+	Division = (
+
+		(ATOM_W, "Women's Atomweight"),
+		(STRAW_W, "Women's Strawweight"),
+		(FLY_W, "Women's Flyweight"),
+		(BANTAM_W, "Women's Bantamweight"),
+		(FEATHER_W, "Women's Featherweight"),
+
+		(FLY_M, "Men's Flyweight"),
+		(BANTAM_M, "Men's Bantamweight"),
+		(FEATHER_M, "Men's Featherweight"),
+		(LIGHT_M, "Men's Lightweight"),
+		(WELTER_M, "Men's Welterweight"),
+		(MIDDLE_M, "Men's Middleweight"),
+		(LIGHT_HEAVY_M, "Men's Light-Heavyweight"),
+		(HEAVY_M, "Men's Heavyweight"),
+	)
+
+	division = models.IntegerField(choices=Division)
+
+	def __str__(self):
+		return self.Division[self.division][1]
+
+class User_Prediction(models.Model):
+	#bout_id = models.ForeignKey(Bout)
+	winner = models.ForeignKey(Fighter)
+	method = models.ForeignKey(Method)
+	round_final = models.PositiveIntegerField()
+	confidence = models.PositiveIntegerField()
+	excitement = models.PositiveIntegerField()
+	attachment = models.PositiveIntegerField()
+	note = models.TextField()
+	ring_user_id = models.ForeignKey(Ring_User)
+
+class Bout(models.Model):
+	fight_card_id = models.ForeignKey(Fight_Card)
+	fighter1 = models.ForeignKey(Fighter, related_name="fighter_1")
+	fighter2 = models.ForeignKey(Fighter, related_name="fighter_2")
+	method_id = models.ForeignKey(Method)
+	weight_class = models.ForeignKey(Weight_Class)
+	max_rounds = models.PositiveIntegerField()
