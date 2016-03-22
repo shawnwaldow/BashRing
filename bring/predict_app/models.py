1ba165c61cb1fbed487c1b0b50a743f02481e95c
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-22 16:02:44 -0700

    Models version 1 complete with dummy data. NExt Steps expand dummy data and dump to begin shaping api transaction!

--- bring/predict_app/models.py
+++ bring/predict_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 #import stuff for AUTH_USER_MODEL
 from django.conf import settings
 
+from datetime import datetime
 # Create your models here.
 
 class Ring_User(models.Model):
@@ -13,6 +14,10 @@ class Ring_User(models.Model):
 	accuracy = models.FloatField(default=1)
 	experience = models.PositiveIntegerField(default=0)
 	avatar = models.ImageField(default="static/menu_images/user1.png",upload_to="avatar_images")
+
+	def __str__(self):
+		return self.first_name+" "+self.last_name
+
 
 class Fighter(models.Model):
 	last_name = models.CharField(max_length=64, default=' ')
@@ -36,37 +41,11 @@ class Fighter(models.Model):
 	water = models.FloatField(default=1)
 	gender = models.BooleanField(default=True) #F=Female, T=Male
 
-class Method(models.Model):
-	#CONST
-	INCOMPLETE = 0
-	SUB = 1
-	TKO = 2 
-	KO = 3
-	UNAN_DEC = 4
-	SPLIT_DEC = 5
-	DRAW = 6
-	NC =  7
-
-	Result = (
-		(INCOMPLETE, 'Incomplete'),
-		(SUB, 'Submission'),
-		(TKO, 'TKO'),
-		(KO, 'KO'),
-		(UNAN_DEC, 'Unanimous Decision'),
-		(SPLIT_DEC, 'Split Decision'),
-		(DRAW, 'Draw'),
-		(NC, 'No Contest')
-	)
-
-	result = models.IntegerField(choices=Result, default=INCOMPLETE)
 
 	def __str__(self):
-		return self.Result[self.result][1]
+		return self.first_name+" '"+self.nick_name+"' "+self.last_name
 
-class Fight_Card(models.Model):
-	title = models.CharField(max_length=127)
-	organization = models.CharField(max_length=64, default='UFC')
-	start_time = models.DateTimeField()
+
 
 class Weight_Class(models.Model):
 	#CONST
@@ -110,6 +89,64 @@ class Weight_Class(models.Model):
 	def __str__(self):
 		return self.Division[self.division][1]
 
+
+
+class Method(models.Model):
+	#CONST
+	INCOMPLETE = 0
+	SUB = 1
+	TKO = 2 
+	KO = 3
+	UNAN_DEC = 4
+	SPLIT_DEC = 5
+	DRAW = 6
+	NC =  7
+
+	Result = (
+		(INCOMPLETE, 'Incomplete'),
+		(SUB, 'Submission'),
+		(TKO, 'TKO'),
+		(KO, 'KO'),
+		(UNAN_DEC, 'Unanimous Decision'),
+		(SPLIT_DEC, 'Split Decision'),
+		(DRAW, 'Draw'),
+		(NC, 'No Contest')
+	)
+
+	result = models.IntegerField(choices=Result, default=INCOMPLETE)
+
+	def __str__(self):
+		return self.Result[self.result][1]
+
+class Fight_Card(models.Model):
+	title = models.CharField(max_length=127)
+	organization = models.CharField(max_length=64, default='UFC')
+	#GMT
+	start_time = models.DateTimeField()
+	end_time = 	models.DateTimeField(blank=True, default=datetime.now())
+	organizations_id = models.PositiveIntegerField(default=0)
+
+	def __str__(self):
+		return self.organization + " " + self.title
+
+
+
+class Bout(models.Model):
+	fight_card_id = models.ForeignKey(Fight_Card)
+	fighter1 = models.ForeignKey(Fighter, related_name="fighter_1")
+	fighter2 = models.ForeignKey(Fighter, related_name="fighter_2")
+	method_id = models.ForeignKey(Method)
+	weight_class = models.ForeignKey(Weight_Class)
+	max_rounds = models.PositiveIntegerField()
+	fighter1_odds = models.IntegerField(default=100)
+	fighter2_odds = models.IntegerField(default=-100)
+	bout_importance_on_card = models.PositiveIntegerField(default=0)
+
+	def __str__(self):
+		return str(self.fight_card_id) +"'s bout: "+ str(self.fighter1) + " vs. "+ str(self.fighter2)
+
+
+
 class User_Prediction(models.Model):
 	#bout_id = models.ForeignKey(Bout)
 	winner = models.ForeignKey(Fighter)
@@ -120,11 +157,18 @@ class User_Prediction(models.Model):
 	attachment = models.PositiveIntegerField()
 	note = models.TextField()
 	ring_user_id = models.ForeignKey(Ring_User)
+	bout_id = models.ForeignKey(Bout, default=0)
+	#can we make a string here to show user name and each fighter name?!
 
-class Bout(models.Model):
-	fight_card_id = models.ForeignKey(Fight_Card)
-	fighter1 = models.ForeignKey(Fighter, related_name="fighter_1")
-	fighter2 = models.ForeignKey(Fighter, related_name="fighter_2")
-	method_id = models.ForeignKey(Method)
-	weight_class = models.ForeignKey(Weight_Class)
-	max_rounds = models.PositiveIntegerField()
+	def __str__(self):
+		return str(self.bout_id) + " Prediction by " + str(self.ring_user_id)
+
+
+
+class BoutWinner(models.Model):
+	bout_id = models.ForeignKey(Bout)
+	fighter_id = models.ForeignKey(Fighter)
+
+
+	def __str__(self):
+		return "bout"+bout_id+":Winner"+fighter_id
