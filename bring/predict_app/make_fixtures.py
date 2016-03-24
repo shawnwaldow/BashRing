8774727feaf40e05f9da5db2611889821f0c1f0e
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-24 16:36:42 -0700

    Ugh. the base UFC fighters api offers too little info to finish the tables! Must scrape, abandon the rest of the fields, or find better apis?

--- bring/predict_app/make_fixtures.py
+++ bring/predict_app/make_fixtures.py
@@ -1,6 +1,9 @@
 import json
 import requests
-
+###################################
+#DANGER WILL ROBINSON. WE HAVE YET TO HANDLE
+#ALL NULLS WITH GRACE!!!
+##################################
 def get_all_data():
     dummy_data=[
         {
@@ -26,9 +29,66 @@ def get_all_data():
             }
         }
     ]
+ 
+    fighter_dummy_data =[
+        {
+        "model": "predict_app.fighter",
+        "pk": 1,
+        "fields": {
+            "last_name": "chump",
+            "first_name": "weak",
+            "nick_name": "glass jaw",
+            "statid": 1,
+            "fighter_status": true,
+            "image": "static/menu_images/anon_fighter_small.jpg",
+            "wins": 0,
+            "losses": 1,
+            "draws": 0,
+            "ncs": 0,
+            "tkos": 0,
+            "kos": 0,
+            "decs": 0,
+            "subs": 0,
+            "days_layoff": 30,
+            "fudge": 1.0,
+            "spice": 1.0,
+            "batwings": 1,
+            "water": 1.0,
+            "gender": true
+            }
+        },
+        {
+        "model": "predict_app.fighter",
+        "pk": 2,
+        "fields": {
+            "last_name": "surley",
+            "first_name": "damien",
+            "nick_name": "stone hands",
+            "statid": 3,
+            "fighter_status": true,
+            "image": "static/menu_images/anon_fighter_small.jpg",
+            "wins": 1,
+            "losses": 0,
+            "draws": 0,
+            "ncs": 0,
+            "tkos": 0,
+            "kos": 1,
+            "decs": 0,
+            "subs": 0,
+            "days_layoff": 30,
+            "fudge": 1.0,
+            "spice": 1.0,
+            "batwings": 1,
+            "water": 1.0,
+            "gender": true
+        }
+        }
+    ]
+
 
 
     output = dummy_data
+    fighter_output = fighter_dummy_data
 
     def get_champ_data():
         url = "http://ufc-data-api.ufc.com/api/v1/us/events/"
@@ -52,6 +112,27 @@ def get_all_data():
             event["fields"]["end_time"] = val["end_event_dategmt"]
             output.append(event)
 
+    def get_fighter_data():
+        url = "http://ufc-data-api.ufc.com/api/v1/us/fighters"
+
+        r = requests.get(url)
+        data = r.json()
+        for val in data:
+            fighter = {}
+            fighter["pk"] = val["id"]
+            fighter["model"] = "predict_app.fighter"
+            fighter["fields"] = {}
+            fighter["fields"]["last_name"] = val["last_name"]
+            fighter["fields"]["first_name"] = val["first_name"]
+            fighter["fields"]["nick_name"] = val[" "]
+            fighter["fields"]["statid"] = val["statid"]
+            fighter["fields"]["fighter_status"] = (val["fighter_status"] == "Active")
+            fighter["fields"]["image"] = "static/menu_images/anon_fighter_small.jpg"
+            fighter["fields"]["wins"] = val["wins"]
+            fighter["fields"]["losses"] = val["losses"]
+            fighter["fields"]["draws"] = val["draws"]
+            fighter["fields"]["ncs"] = 0
+            fighter_output.append(fighter)
 
     get_champ_data()
     
