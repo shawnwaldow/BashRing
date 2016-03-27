7085488650ccb3d93df86e0447f3a440d41b86e9
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-26 21:49:30 -0700

    Experimenting with several ways to access objects and variables in django templates. Got stumped on adding html tags by simple string concatenation.

--- bring/predict_app/views.py
+++ bring/predict_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404 #we added the last one fo
 from django.http.response import HttpResponse
 
 #Allow getting things from DB
-from .models import Fight_Card, Fighter
+from .models import Fight_Card, Fighter, Bout
 
 from django.http import JsonResponse
 
@@ -21,9 +21,21 @@ def display_fight_card(request, fight_card_id=1):
 	fight_card = get_object_or_404(Fight_Card, pk=fight_card_id)
 	fighter1 = get_object_or_404(Fighter, pk=1)
 	fighter2 = get_object_or_404(Fighter, pk=2)
+	array = [0,1,2,3,4]
+	main_bout = get_object_or_404(Bout, pk=1)
+	bout_string = preview_a_bout(main_bout)
+
 
 	context = {
-		'fight_card': fight_card, 'fighter1': fighter1, 'fighter2': fighter2
+		'fight_card': fight_card, 'fighter1': fighter1, 'fighter2': fighter2, 'array': array, 'bout': get_object_or_404(Bout, pk=1), 'main_bout': main_bout
 	}
 
 	return render(request, 'predict_app/predict_a_card.html', context)
+
+def preview_a_bout(a_bout):
+	"""Displays an overview of a bout."""
+	#fighter1 = get_object_or_404(Fighter, pk=1)
+	#fighter2 = get_object_or_404(Fighter, pk=2)
+
+	bout_span=a_bout.fighter1.last_name+" vs. "+a_bout.fighter2.last_name
+	return bout_span
